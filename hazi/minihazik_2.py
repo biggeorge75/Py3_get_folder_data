683af6346b86ddc931ae78aefbb1978867d8693c
ted <apple.tibor@gmail.com>
2019-10-11 12:18:00 +0200

    Miniházik 2

---
+++ hazi/minihazik_2.py
@@ -0,0 +1,32 @@
+# # 1. Írj egy olyan függvényt amely visszaadja három szám közül a legnagyobbat. A megoldásban nem használhatsz standard lib. függvényeket pl: max()
+
+# def biggest(a, b, c):
+#     if a > b and a > c:
+#         return a
+#     if b > a and b > c:
+#         return b
+#     if c > a and c > b:
+#         return c
+#
+# print(biggest(40, 43, 161))
+########################################################################################################################
+########################################################################################################################
+# # 2. Írj egy olyan függvényt amely összead minden olyan számot egy listában amely páros szám.
+
+# def add_even_number_list(lista):
+#     number = sum(list(filter(lambda x: x % 2 == 0, lista)))
+#     return number
+#
+# lista = [ 2, 4, 3, 5, 4, 11, 8]
+# print(add_even_number_list(lista))
+########################################################################################################################
+########################################################################################################################
+# # 3. Írj egy olyan függvényt amely megfordít egy string-et. Pl.:1234abcd >>> dcba4321
+
+# def invert(word):
+#     return word[::-1]
+#
+# print(invert('1234abcd'))
+########################################################################################################################
+########################################################################################################################
+
